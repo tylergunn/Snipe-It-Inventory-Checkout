477d3aaaa524b0aea643b640e807cef90657636a
Tyler Gunn <tylergunn@reedssports.com>
2022-11-22 09:35:38 -0600

    Test

--- server.py
+++ server.py
@@ -73,4 +73,4 @@ def checkout():
     
     return render_template('checkout.html', form=request.form, colours=colours )
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=443)
+    app.run(host='0.0.0.0')
